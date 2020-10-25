eba5fbce1238e3d48cf1e3dfd16d8ea9d2b79ce2
Dzmitry Aksiantsiuk <dzmitry.aksiantsiuk@gmail.com>
2020-10-25 21:45:53 +0300

    feat: implement function reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    const value = String(n).split('');
+    if (value[0] === '-') {
+        return Number(value.slice(1).reverse().join(""));
+    } 
+    if (value[value.length - 1] === "0") {
+        return Number(value.slice(0, value.length - 1).reverse().join(""));
+    }
+
+    return Number(value.reverse().join(''));
 }
